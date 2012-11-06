88fb0b3918825441b8633b95d8e3b09bccfd4e8b
Harrison Coatney <jhcoatney@gmail.com>
2012-11-05 19:52:36 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BurritoBros::Application.config.secret_token = '04bc29ddb0666807b9c65f3b1d8efee3c581a98fb81d1d39d450c7ac07d023cef9056e749f77965a907a55fefb455a345996f63d9cd6391f8dcc542129547bd3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BurritoBros::Application.config.session_store :cookie_store, key: '_BurritoBros_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# BurritoBros::Application.config.session_store :active_record_store
